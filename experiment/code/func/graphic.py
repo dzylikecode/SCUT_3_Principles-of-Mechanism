0bf68ff82cf506d60182ca0eb34c62210b1c48f6
DZ <1494977853@qq.com>
2022-01-18 18:12:44 +0800

    fix: test pict in comment

--- experiment/code/func/graphic.py
+++ experiment/code/func/graphic.py
@@ -1,2 +1,46 @@
 from matplotlib.patches import Ellipse, Circle
 import matplotlib.pyplot as plt
+
+
+class Fig_position:
+    """绘画机构位置简图"""
+    def __init__(self, cood_param, A, B, r):
+        self.cood_param = cood_param
+        self.A = A
+        self.B = B
+        self.r = r
+        fig = plt.figure()
+        self.ax = fig.add_subplot(111)
+
+    def __draw_O(self):
+        x, y = 0, 0
+        self.ax.plot(x, y, 'ro')
+
+    def __draw_circles(self):
+        cir = Circle(xy=(0.0, 0.0), radius=self.r, alpha=0.5)
+        self.ax.add_patch(cir)
+
+    def __draw_crank(self, A):
+        self.ax.plot([0, A['x']], [0, A['y']], color="red")
+
+    def __draw_link(self, A, B):
+        self.ax.plot([A['x'], B['x']], [A['y'], B['y']], color="green")
+
+    def __draw_slider(self):
+        self.ax.plot([self.cood_param.B_up['x'], self.cood_param.B_down['x']],
+                     [self.cood_param.B_up['y'], self.cood_param.B_down['y']],
+                     color="red",
+                     linestyle="--")
+
+    def show(self, id_name):
+        self.ax.set_title(id_name, fontsize=12, color='r')
+        self.__draw_O()
+        self.__draw_circles()
+        self.__draw_crank(self.A)
+        self.__draw_link(self.A, self.B)
+        self.__draw_slider()
+
+        plt.axis('scaled')
+        #changes limits of x or y axis so that equal increments of x and y have the same length
+        plt.axis('equal')
+        plt.show()
--- experiment/code/func/tool.py
+++ experiment/code/func/tool.py
@@ -1,4 +1,6 @@
 import cmath
+import sympy
+import math
 
 
 def xy_to_polar_theta(x, y):
@@ -8,3 +10,45 @@ def xy_to_polar_theta(x, y):
     z = complex(x, y)
     (_, theta) = cmath.polar(z)
     return theta
+
+
+class Vector_equation:
+    """
+    ## import
+
+    PA: 已知的向量 (包括 `角度` 和 `方向`)
+    theta_AB: 已知向量的 `角度` (`弧度`)
+    theta_PB: 已知向量的 `角度` (`弧度`)
+
+    ![](../pict/2.png)
+
+    ### Method
+
+    1. 以 P 点为原点
+    2. AB 的参数方程
+    3. PB 的参数方程
+    4. 联立参数方程 => 求解出 B 点位置
+    """
+    def __init__(self, PA, theta_AB, theta_PB):
+        self.PA = PA
+        self.theta_AB = theta_AB
+        self.theta_PB = theta_PB
+        self.x = 0.0
+        self.y = 0.0
+
+        self.__solve()
+
+    def __solve(self):
+
+        # x,y 为 PB 与 AB 的交点
+        x, y, t1, t2 = sympy.symbols('x, y, t1, t2')
+        # AB 的参数方程 交于交点
+        fun_AB_x = self.PA.x + t1 * math.cos(self.theta_AB) - x
+        fun_AB_y = self.PA.y + t1 * math.sin(self.theta_AB) - y
+        # 同理 PB
+        fun_PB_x = t2 * math.cos(self.theta_PB) - x
+        fun_PB_y = t2 * math.sin(self.theta_PB) - y
+        result = sympy.solve([fun_AB_x, fun_AB_y, fun_PB_x, fun_PB_y],
+                             [x, y, t1, t2])
+        self.x = result[x]
+        self.y = result[y]
